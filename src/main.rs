9cb279f095cee61b3bc16fb3dd7eb529d3c770be
Stefan Kecskes <mr.kecskes@gmail.com>
2024-12-12 23:59:03 +0000

    progress on day 9

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 mod day0;
 mod day2;
 mod day5;
+mod day9;
 
 use axum::{Router};
 use crate::day0::day0_routes;
 use crate::day2::day2_routes;
 use crate::day5::day5_routes;
+use crate::day9::day9_routes;
 
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
@@ -13,5 +15,6 @@ async fn main() -> shuttle_axum::ShuttleAxum {
     let router = day0_routes(router);
     let router = day2_routes(router);
     let router = day5_routes(router);
+    let router = router.merge(day9_routes());
     Ok(router.into())
 }
