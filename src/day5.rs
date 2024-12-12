633ed926c2939fad1633c9fc82f36b8e09481872
Stefan Kecskes <mr.kecskes@gmail.com>
2024-12-12 23:27:42 +0000

    all tasks in day5

--- src/day5.rs
+++ src/day5.rs
@@ -1,79 +1,95 @@
+use std::fmt;
 use axum::http::{HeaderMap, StatusCode};
 use axum::response::IntoResponse;
 use axum::Router;
 use axum::routing::post;
 use cargo_manifest::Manifest;
 use serde::Deserialize;
+use serde_with::serde_as;
 use shuttle_runtime::__internals::serde_json;
-
-#[derive(Deserialize)]
-struct Package {
-    name: String,
-    #[serde(default)]
-    keywords: Vec<String>,
-    #[serde(default)]
-    metadata: Metadata,
-}
 
 #[derive(Deserialize, Default)]
 struct Metadata {
-    orders: Vec<Order>,
+    #[serde(default)]
+    pub orders: Vec<Order>,
 }
 
-#[serde_with::serde_as]
-#[derive(Deserialize)]
+#[serde_as]
+#[derive(Deserialize, Debug)]
 struct Order {
-    item: String,
+    pub item: String,
     #[serde_as(deserialize_as = "serde_with::DefaultOnError")]
     #[serde(default)]
-    quantity: Option<usize>,
+    pub quantity: Option<u32>,
+}
+
+impl fmt::Display for Order {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self.quantity {
+            Some(num) => write!(f, "{}: {:?}", self.item, num),
+            None => write!(f, ""),
+        }
+    }
 }
 
 async fn toml_orders(headers: HeaderMap, text: String) -> impl IntoResponse {
-
-    let Some(package) = (match headers.get("content-type") {
+    let manifest = match headers.get("content-type") {
         Some(content_type) => {
             match content_type.to_str() {
                 Ok("application/toml") => {
-                    toml::from_str::<Manifest<Metadata>>(&text).ok()
-                },
+                    if let Ok(manifest) = toml::from_str::<Manifest<Metadata>>(&text) {
+                        manifest
+                    } else {
+                        return (StatusCode::BAD_REQUEST, "Invalid manifest".to_string());
+                    }
+                }
                 Ok("application/json") => {
-                    serde_json::from_str::<Manifest<Metadata>>(&text).ok()
-                },
+                    if let Ok(manifest) = serde_json::from_str::<Manifest<Metadata>>(&text) {
+                        manifest
+                    } else {
+                        return (StatusCode::BAD_REQUEST, "Invalid manifest".to_string());
+                    }
+                }
                 Ok("application/yaml") => {
-                    serde_yaml::from_str::<Manifest<Metadata>>(&text).ok()
-                },
+                    if let Ok(manifest) = serde_yaml::from_str::<Manifest<Metadata>>(&text) {
+                        manifest
+                    } else {
+                        return (StatusCode::BAD_REQUEST, "Invalid manifest".to_string());
+                    }
+                }
                 _ => return (StatusCode::UNSUPPORTED_MEDIA_TYPE, "Unsupported content type".to_string())
             }
-        },
+        }
         None => return (StatusCode::UNSUPPORTED_MEDIA_TYPE, "Content type not set".to_string()),
-    }).and_then(|manifest| manifest.package) else {
-        return (StatusCode::BAD_REQUEST, "Invalid manifest".to_string());
     };
 
+    let package = manifest.package.unwrap();
     if !package
         .keywords
-        .and_then(|kws| kws.as_local())
-        .map(|kws| kws.contains(&"Christmas 2024".to_string()))
-        .unwrap_or_default() {
+        .and_then(|k| k.as_local())
+        .map(|k| k.contains(&"Christmas 2024".to_string()))
+        .unwrap_or_default()
+    {
         return (StatusCode::BAD_REQUEST, "Magic keyword not provided".to_string());
     }
 
-    let Some(orders) = package.metadata.map(|m| {
-        m.orders.into_iter()
-            .filter(|order| order.quantity.is_some())
-            .map(|order| {
-                format!("{}: {}", order.item, order.quantity.unwrap())
-            })
-            .collect::<Vec<_>>()
-        }) else {
-            return (StatusCode::NO_CONTENT, "No orders found".to_string());
-        };
+    let metadata = match package.metadata {
+        Some(metadata) => metadata,
+        None => return (StatusCode::NO_CONTENT, "No metadata found".to_string()),
+    };
+
+    let orders: String = metadata
+        .orders
+        .iter()
+        .filter(|order| order.quantity.is_some())
+        .map(|order| order.to_string())
+        .collect::<Vec<_>>()
+        .join("\n");
 
     if orders.is_empty() {
         return (StatusCode::NO_CONTENT, "No orders found".to_string());
     }
-    (StatusCode::OK, orders.join("\n"))
+    (StatusCode::OK, orders)
 }
 
 pub fn day5_routes(router: Router) -> Router {
